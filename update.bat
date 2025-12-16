4a308c08f191382cee185ae80afdc19430065bf1
1506022022 <vmap313@gmail.com>
2025-12-16 21:50:52 +0900

    chore: 깃 업데이트 자동화 도구 업로드
    - 실행시 root 리포지토리 및 Assets 폴더 안의 모든 하위 리포지토리 일괄 업로드

---
+++ update.bat
@@ -0,0 +1,28 @@
+@echo off
+git fetch
+echo [main] git fetch
+
+
+setlocal enabledelayedexpansion
+
+cd /d "%~dp0Assets" || (
+    echo not exits Assets folder
+    pause
+    exit /b 1
+)
+
+for /d %%D in (*) do (
+    if exist "%%D\.git" (
+        echo.
+        echo [%%D] git fetch
+        pushd "%%D"
+        git fetch
+        popd
+    ) else (
+        echo.
+        echo [%%D] not git repository
+    )
+)
+
+echo.
+pause
